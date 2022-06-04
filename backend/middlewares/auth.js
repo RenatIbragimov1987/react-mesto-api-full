b9196bf8b76ad0740868defb2c737f858e80211a
Renat Ibragimov <naka070787@gmail.com>
2022-06-04 15:12:43 +0300

    commit

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,21 +1,42 @@
+// const jwt = require('jsonwebtoken');
+// // const JWT_SECRET = require('../utils/jwt');
+// const UnauthorizedError = require('../errors/UnauthorizedError');
+
+// const JWT_SECRET = 'IAMSECRETPASSWORD';
+
+// const isAuth = async (req, res, next) => {
+//   const token = req.cookies.jwt;
+//   let payload;
+//   try {
+//     payload = jwt.verify(token, JWT_SECRET);
+//     req.userId = jwt.decode(token).id;
+//   } catch (err) {
+//     next(new UnauthorizedError('Ошибка. Необходима авторизация'));
+//     return;
+//   }
+//   req.user = payload;
+//   next();
+// };
+
+// module.exports = isAuth;
+
 const jwt = require('jsonwebtoken');
-// const JWT_SECRET = require('../utils/jwt');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const JWT_SECRET = 'IAMSECRETPASSWORD';
-
-const isAuth = async (req, res, next) => {
+module.exports = async (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
+
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    // при продакшене используем JWT_SECRET
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
     req.userId = jwt.decode(token).id;
   } catch (err) {
-    next(new UnauthorizedError('Ошибка. Необходима авторизация'));
+    next(new UnauthorizedError('Необходима авторизация'));
     return;
   }
-  req.user = payload;
-  next();
+  req.user = payload; // записываем пейлоуд в объект запроса
+  next(); // пропускаем запрос дальше
 };
-
-module.exports = isAuth;
--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,8 +1,19 @@
-const JWT_SECRET = 'IAMSECRETPASSWORD';
+// const JWT_SECRET = 'IAMSECRETPASSWORD';
+// const jwt = require('jsonwebtoken');
+
+// const getToken = async (id) => jwt.sign({ id }, JWT_SECRET, { expiresIn: '7d' });
+// module.exports = {
+//   getToken,
+//   JWT_SECRET,
+// };
+
 const jwt = require('jsonwebtoken');
 
-const getToken = async (id) => jwt.sign({ id }, JWT_SECRET, { expiresIn: '7d' });
-module.exports = {
-  getToken,
-  JWT_SECRET,
-};
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+// JSON Web Token
+// JSON объект закодированный с помощью секрета JWT_SECRET (пока простой как в тренажере)
+
+const getToken = async (id) => jwt.sign({ id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
+
+module.exports = { getToken };
