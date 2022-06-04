f76bb08ee5af481728b1b741c13465ab09ae2468
Renat Ibragimov <naka070787@gmail.com>
2022-06-05 00:03:02 +0300

    commit

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const isAuth = require('./middlewares/auth');
 
 require('dotenv').config();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const { login, createUser } = require('./controllers/users');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
