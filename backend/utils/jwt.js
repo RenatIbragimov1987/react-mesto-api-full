b1b722719356625a7d34ca2eee5e0a18f95643fe
Renat Ibragimov <naka070787@gmail.com>
2022-06-09 04:59:02 +0300

    commit

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -14,6 +14,6 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 // JSON Web Token
 // JSON объект закодированный с помощью секрета JWT_SECRET (пока простой как в тренажере)
 
-const getToken = async (id) => jwt.sign({ id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
+const getToken = async (id) => jwt.sign({ id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '365d' });
 
 module.exports = { getToken };
