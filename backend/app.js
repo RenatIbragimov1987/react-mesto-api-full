69d5c4bfc4f8f0154caa565055be36fc52a7e537
Renat Ibragimov <naka070787@gmail.com>
2022-06-09 17:38:19 +0300

    comm

--- backend/app.js
+++ backend/app.js
@@ -17,8 +17,8 @@ const NotFoundDataError = require('./errors/NotFoundDataError');
 const app = express();
 
 const accessCors = [
-  'https://api.renat1987.nomoredomains.xyz',
-  'http://api.renat1987.nomoredomains.xyz',
+  'https://renat.domains.nomoredomains.sbs',
+  'http://renat.domains.nomoredomains.sbs',
   'http://localhost:3001',
   'https://localhost:3001',
 ];
