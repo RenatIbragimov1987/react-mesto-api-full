0bffeedb9d38459861503503580ab7365658bab7
Renat Ibragimov <naka070787@gmail.com>
2022-06-09 18:52:53 +0300

    commit

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,77 +1,66 @@
-import BASE_URL from './utils';
-class Auth {
-  constructor({ address }) {
-    this._backendUrl = address;
+import BASE_URL from "./utils";
+
+export const checkResponse = (res) => {
+  if (res.ok) {
+    return res.json();
   }
+  return Promise.reject(`Error: ${res.status}`);
+};
 
-  _checkResponse = (res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Error: ${res.status}`);
-  };
+export const userRegistration = (email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ email, password }),
+  }).then(this._checkResponse);
+};
 
-  userRegistration = (email, password) => {
-    return fetch(`${this._backendUrl}/signup`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-			credentials: 'include',
-      body: JSON.stringify({email, password}),
-    }).then(this._checkResponse);
-  };
+export const userAuthorization = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ email, password }),
+  }).then(this._checkResponse);
+};
 
-  userAuthorization = (email, password) => {
-    return fetch(`${this._backendUrl}/signin`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-			credentials: 'include',
-      body: JSON.stringify({email, password}),
-    }).then(this._checkResponse);
-  };
+export const signout = () => {
+  return fetch(`${BASE_URL}/signout`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  }).then(this._checkResponse);
+};
 
-	signout = () => {
-    return fetch(`${this._backendUrl}/signout`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        credentials: 'include',
-    }).then(this._checkResponse);
-	};
+export const getContent = () => {
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  }).then(this._checkResponse);
+};
 
-	getContent = () => {
-    return fetch(`${this._backendUrl}/users/me`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        credentials: 'include',
-    }).then(this._checkResponse);
-	};
-
-  // userToken = (token) => {
-  //   return fetch(`${this._backendUrl}/users/me`, {
-  //     method: "GET",
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //       Authorization: `Bearer ${this._backendUrl}`,
-  //     },
-	// 		credentials: 'include',
-  //   }).then(this._checkResponse);
-  // };
-}
-
-const auth = new Auth({
-  address: BASE_URL
-});
-
-export default auth;
+// userToken = (token) => {
+//   return fetch(`${this._backendUrl}/users/me`, {
+//     method: "GET",
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${this._backendUrl}`,
+//     },
+// 		credentials: 'include',
+//   }).then(this._checkResponse);
+// };
