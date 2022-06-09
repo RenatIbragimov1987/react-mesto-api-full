ec50f5daa43fa188fdb8651f965c10592bfc8e37
Renat Ibragimov <naka070787@gmail.com>
2022-06-09 17:16:52 +0300

    commit

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,42 +1,41 @@
-const jwt = require('jsonwebtoken');
-// const JWT_SECRET = require('../utils/jwt');
-const UnauthorizedError = require('../errors/UnauthorizedError');
-
-const JWT_SECRET = 'IAMSECRETPASSWORD';
-
-const isAuth = async (req, res, next) => {
-  const token = req.cookies.jwt;
-  let payload;
-  try {
-    payload = jwt.verify(token, JWT_SECRET);
-    req.userId = jwt.decode(token).id;
-  } catch (err) {
-    next(new UnauthorizedError('Ошибка. Необходима авторизация'));
-    return;
-  }
-  req.user = payload;
-  next();
-};
-
-module.exports = isAuth;
-
 // const jwt = require('jsonwebtoken');
-
-// const { NODE_ENV, JWT_SECRET } = process.env;
+// // const JWT_SECRET = require('../utils/jwt');
 // const UnauthorizedError = require('../errors/UnauthorizedError');
 
-// module.exports = async (req, res, next) => {
+// const JWT_SECRET = 'IAMSECRETPASSWORD';
+
+// const isAuth = async (req, res, next) => {
 //   const token = req.cookies.jwt;
 //   let payload;
-
 //   try {
-//     // при продакшене используем JWT_SECRET
-//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+//     payload = jwt.verify(token, JWT_SECRET);
 //     req.userId = jwt.decode(token).id;
 //   } catch (err) {
-//     next(new UnauthorizedError('Необходима авторизация'));
+//     next(new UnauthorizedError('Ошибка. Необходима авторизация'));
 //     return;
 //   }
-//   req.user = payload; // записываем пейлоуд в объект запроса
-//   next(); // пропускаем запрос дальше
+//   req.user = payload;
+//   next();
 // };
+
+// module.exports = isAuth;
+
+const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+module.exports = async (req, res, next) => {
+  const token = req.cookies.jwt;
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+    req.userId = jwt.decode(token).id;
+  } catch (err) {
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
+  }
+  req.user = payload; // записываем пейлоуд в объект запроса
+  next(); // пропускаем запрос дальше
+};
