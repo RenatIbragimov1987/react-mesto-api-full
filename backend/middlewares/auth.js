5eb1a87f4e8b9fc7e44f92d7a855c29345250ff7
Renat Ibragimov <naka070787@gmail.com>
2022-06-11 00:21:31 +0300

    comm

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,3 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const { REACT_APP_NODE_ENV, REACT_APP_JWT_SECRET } = process.env;
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+module.exports = async (req, res, next) => {
+  const token = req.cookies.jwt;
+  let payload;
+
+  try {
+    payload = jwt.verify(token, REACT_APP_NODE_ENV === 'production' ? REACT_APP_JWT_SECRET : 'efed42a609a1be1e1ba62ba374dd21e324364d309a998156a2bee646bd541cc0');
+    req.userId = jwt.decode(token).id;
+  } catch (err) {
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
+  }
+  req.user = payload; // записываем пейлоуд в объект запроса
+  next(); // пропускаем запрос дальше
+};
+
 // const jwt = require('jsonwebtoken');
 // // const JWT_SECRET = require('../utils/jwt');
 // const UnauthorizedError = require('../errors/UnauthorizedError');
@@ -19,23 +39,3 @@
 // };
 
 // module.exports = isAuth;
-
-const jwt = require('jsonwebtoken');
-
-const { REACT_APP_NODE_ENV, REACT_APP_JWT_SECRET } = process.env;
-const UnauthorizedError = require('../errors/UnauthorizedError');
-
-module.exports = async (req, res, next) => {
-  const token = req.cookies.jwt;
-  let payload;
-
-  try {
-    payload = jwt.verify(token, REACT_APP_NODE_ENV === 'production' ? REACT_APP_JWT_SECRET : 'efed42a609a1be1e1ba62ba374dd21e324364d309a998156a2bee646bd541cc0');
-    req.userId = jwt.decode(token).id;
-  } catch (err) {
-    next(new UnauthorizedError('Необходима авторизация'));
-    return;
-  }
-  req.user = payload; // записываем пейлоуд в объект запроса
-  next(); // пропускаем запрос дальше
-};
--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -14,6 +14,6 @@ const { REACT_APP_NODE_ENV, REACT_APP_JWT_SECRET } = process.env;
 // JSON Web Token
 // JSON объект закодированный с помощью секрета JWT_SECRET (пока простой как в тренажере)
 
-const getToken = async (id) => jwt.sign({ id }, REACT_APP_NODE_ENV === 'production' ? REACT_APP_JWT_SECRET : 'efed42a609a1be1e1ba62ba374dd21e324364d309a998156a2bee646bd541cc0', { expiresIn: '7d' });
+const getToken = async (id) => jwt.sign({ id }, REACT_APP_NODE_ENV === 'production' ? REACT_APP_JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
 
 module.exports = { getToken };
