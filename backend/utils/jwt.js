65000295b9ead6e458c43cacc8ea48c3ee4a990a
Renat Ibragimov <naka070787@gmail.com>
2022-06-11 03:04:56 +0300

    comm

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -7,10 +7,10 @@
 //   JWT_SECRET,
 // };
 
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-const { REACT_APP_NODE_ENV, REACT_APP_JWT_SECRET } = process.env;
+// const { REACT_APP_NODE_ENV, REACT_APP_JWT_SECRET } = process.env;
 
-const getToken = async (id) => jwt.sign({ id }, REACT_APP_NODE_ENV === 'production' ? REACT_APP_JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
+// const getToken = async (id) => jwt.sign({ id }, REACT_APP_NODE_ENV === 'production' ? REACT_APP_JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
 
-module.exports = { getToken };
+// module.exports = { getToken };
