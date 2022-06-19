0af76d49b9da27e2a34c8b14dfb4492f5a80e92e
Renat Ibragimov <naka070787@gmail.com>
2022-06-19 23:19:53 +0300

    comm

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 require('dotenv').config();
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 const { login, createUser } = require('./controllers/users');
 const { users } = require('./routes/users');
 const { cards } = require('./routes/cards');
@@ -20,7 +20,7 @@ const app = express();
 const accessCors = [
   'https://renat.domains.nomoredomains.sbs',
   'http://renat.domains.nomoredomains.sbs',
-  'http://localhost:3000',
+  'http://localhost:3001',
 ];
 
 const options = {
