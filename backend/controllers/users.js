d60784cc848cde18537f22e723483e81d34ae722
Renat Ibragimov <naka070787@gmail.com>
2022-06-26 13:59:02 +0300

    comm

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -102,7 +102,7 @@ const login = async (req, res, next) => {
       maxAge: 3600000 * 24 * 7,
       httpOnly: true,
       sameSite: 'None',
-      secure: true,
+      secure: false,
     });
     res.status(200).send({ token });
     return;
