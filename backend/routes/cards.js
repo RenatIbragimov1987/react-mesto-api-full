c0a7f80358d7b79c562f62f97b1cd7ba46c883d0
Renat Ibragimov <naka070787@gmail.com>
2022-07-04 22:47:31 +0300

    comm

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -18,7 +18,7 @@ cards.post('/cards', celebrate({
   }),
 }), createCard);
 
-cards.delete('/cards/:cardId', celebrate({
+cards.delete('/cards/:cardId/delete-card', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().hex().length(24).required(),
   }),
